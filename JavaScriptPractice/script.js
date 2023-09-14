872d71d0aeccba292d93ac7fd738aa686daa794b
soumilnimbalkar26 <soumilnimbalkar@gmail.com>
2023-09-14 16:59:02 +0530

    promises

--- JavaScriptPractice/script.js
+++ JavaScriptPractice/script.js
@@ -313,3 +313,66 @@
 // );
 
 //Chaining promises
+
+// p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("resolved after 2 seconds");
+//     resolve(56);
+//   }, 2000);
+// });
+
+// p1.then((value) => {
+//   console.log(value);
+//   let p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Promise 2");
+//     }, 2000);
+//   });
+//   return p2;
+// }).then((value) => {
+//   console.log("we are done");
+// });
+
+//loadscript funtion quiz
+
+// const loadScript = (src) => {
+//   return new Promise((resolve, reject) => {
+//     let script = document.createElement("script");
+//     script.type = "text/javascript";
+//     script.src = src;
+//     document.body.appendChild(script);
+//     script.onload = () => {
+//       resolve(1);
+    //   console.log("script loaded");
+    // };
+    // script.onerror = () => {
+    //   reject(0);
+    //   console.log("error");
+//     };
+//   });
+// };
+
+// let p1 = loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js");
+// p1.then((value) => {
+//   console.log(value);
+// }).catch((error)=>{
+//     console.log("there was an error");
+// })
+
+//multiple handlers in promises
+// let p1 = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         alert("hey i am not resolve")
+//         resolve(1)
+//     },2000)
+// })
+
+// p1.then(()=>{
+//     console.log("congratulations this promise is now resolved");
+// })
+
+// p1.then(()=>{
+//     alert("Hurray");
+// })
+
+//promise API
